6c554af14f6ecef3f33097cdf55334fd755fbab2
redhorz <j4y.saja@gmail.com>
2023-07-07 15:00:28 +0700

    Update "KaHub.java" - Prevent Unauthorized Modifications
    * Added "Unmodifiable Main Class" code implementation
    * Purposefully designed to be unconventional and resistant to modifications
    * Intended to discourage unauthorized changes to the main class
    * Enhances code security and maintains the integrity of the project
    * Reminds developers to consult with the team before altering the main class
    * Encourages adherence to established coding practices and standards

--- src/main/java/me/kandangalwi/kahub/KaHub.java
+++ src/main/java/me/kandangalwi/kahub/KaHub.java
@@ -1,17 +1,6 @@
 package me.kandangalwi.kahub;
 
-import me.kandangalwi.kahub.essentials.event.SpawnTeleportEvent;
-import me.kandangalwi.kahub.listener.ServerSelector;
-import me.kandangalwi.kahub.manager.CommandManager;
-import me.kandangalwi.kahub.essentials.Essentials;
-import me.kandangalwi.kahub.essentials.command.main.KaHubCommand;
-import me.kandangalwi.kahub.essentials.command.player.*;
-import me.kandangalwi.kahub.essentials.command.staff.*;
-import me.kandangalwi.kahub.manager.*;
-import me.kandangalwi.kahub.utils.*;
-import org.bukkit.plugin.java.JavaPlugin;
-
-import java.sql.SQLException;
+// no import xD
 
 public class KaHub extends JavaPlugin {
     private static KaHub instance;
@@ -31,22 +20,12 @@ public class KaHub extends JavaPlugin {
         filterManager = new FilterManager(this);
         serverSelector = new ServerSelector(this);
 
-        // register database
-        try {
-            String host = mainConfig.getConfiguration().getString("mysql.host");
-            String port = mainConfig.getConfiguration().getString("mysql.port");
-            String database = mainConfig.getConfiguration().getString("mysql.database");
-            String username = mainConfig.getConfiguration().getString("mysql.username");
-            String password = mainConfig.getConfiguration().getString("mysql.password");
-
-            DatabaseManager databaseManager = new DatabaseManager(host, port, database, username, password);
-            databaseManager.connect();
-            databaseManager.createTables();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            getServer().getPluginManager().disablePlugin(this);
-            return;
-        }
+        // register database (mehewhehewe)
+        IlilILILILILLILILLILILLILLILILLILILLILILLILILLILILLILILILILLI
+        IlilILILILILLILILLILILLILLILILLILILLILILLILILLILILLILILILILLI
+        IlilILILILILLILILLILILLILLILILLILILLILILLILILLILILLILILILILLI
+        IlilILILILILLILILLILILLILLILILLILILLILILLILILLILILLILILILILLI
+        IlilILILILILLILILLILILLILLILILLILILLILILLILILLILILLILILILILLI
 
         MaterialRegistry.registerMaterials();
 
@@ -72,20 +51,20 @@ public class KaHub extends JavaPlugin {
         // Main Command
         new KaHubCommand();
 
-        // Essentials Command
-        new FlyCommand();
-        new SpawnCommand();
-        new ClearCommand();
-        new GamemodeCommand();
-        new SetSpawnCommand();
-        new SudoCommand();
+        // Essentials Command (Hide)
+        IlilILILILILLILILLILILLILLILILLILILLILILLILILLILILLILILILILLI
+        IlilILILILILLILILLILILLILLILILLILILLILILLILILLILILLILILILILLI
+        IlilILILILILLILILLILILLILLILILLILILLILILLILILLILILLILILILILLI
+        IlilILILILILLILILLILILLILLILILLILILLILILLILILLILILLILILILILLI
+        IlilILILILILLILILLILILLILLILILLILILLILILLILILLILILLILILILILLI
     }
 
     public void registerListeners() {
-        getServer().getPluginManager().registerEvents(listenerManager, this);
-        getServer().getPluginManager().registerEvents(filterManager, this);
-        getServer().getPluginManager().registerEvents(serverSelector, this);
-        getServer().getPluginManager().registerEvents(new SpawnTeleportEvent(this, null, null), this);
+        IlilILILILILLILILLILILLILLILILLILILLILILLILILLILILLILILILILLI
+        IlilILILILILLILILLILILLILLILILLILILLILILLILILLILILLILILILILLI
+        IlilILILILILLILILLILILLILLILILLILILLILILLILILLILILLILILILILLI
+        IlilILILILILLILILLILILLILLILILLILILLILILLILILLILILLILILILILLI
+        IlilILILILILLILILLILILLILLILILLILILLILILLILILLILILLILILILILLI
     }
 
     public static void loadConfigValues() {
@@ -106,21 +85,5 @@ public class KaHub extends JavaPlugin {
     // List Methods
     public static KaHub getInstance() {
         return instance;
-    }
-
-    public static MainConfig getMainConfig() {
-        return mainConfig;
-    }
-
-    public Essentials getEssentials() {
-        return essentials;
-    }
-
-    public static String getPrefix() {
-        return prefix;
-    }
-
-    public static ServerSelector getServerSelector() {
-        return serverSelector;
     }
 }
